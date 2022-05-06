a9eb896a44f0b235ef09c053ec398184e6b6ddf4
Elina <8gelina@gmail.com>
2022-05-06 20:31:38 +0300

    search

--- anilez/food/urls.py
+++ anilez/food/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import Search
 
 urlpatterns = [
     path('', views.home, name='index'),
     path('menu/', views.FoodListView.as_view(), name='menu'),
     path('menuwaiter/', views.FoodPriceListView.as_view(), name='menuwaiter'),
+    path('search/', Search.as_view(), name='search'),
 ]
--- anilez/food/views.py
+++ anilez/food/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -33,3 +34,14 @@ class FoodPriceListView(ListView):
 def menuwaiter(request):
     return render(request, 'menu_for_waiter.html')
 
+class Search(ListView):
+    template_name = 'search.html'
+    context_object_name = 'foods'
+
+    def get_queryset(self):
+        q=self.request.GET.get('s')
+        return Food.objects.filter(Q(title__iregex=q))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context=super().get_context_data(**kwargs)
+        return context
